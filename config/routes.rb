167040624f5ba2ab0dfd6fc48c8c368d5c2fda89
Vishal Singh <vishalsingh.738546@gmail.com>
2021-04-05 19:19:50 +0530

    new custom font added

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,10 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'welcome#index'
 
+  
+
   # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  get 'products' => 'welcome#view'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
